bd612baa09ab8935579b48aede1ec31d41a347fe
unknown <yashkalavadiya1010@gmail.com>
2020-06-26 02:00:04 +0530

    new feature to add updates added

--- src/Admin/Dashboard.js
+++ src/Admin/Dashboard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useEffect } from 'react';
 
+//uuid import
+import uuid from 'react-native-uuid';
+
 //import firebase
 import * as firebase from 'firebase'
 import { Redirect } from 'react-router-dom';
@@ -8,11 +11,19 @@ import { useState } from 'react';
 import Main from '../ReusableComponents/Main';
 
 const Dashboard = () => {
-    const [didRedirect,setDidRedirect] = useState(false)
+    const [didRedirect,setDidRedirect] = useState(false)    
     const [screens,setScreens] = useState({
         isNewNews:false,
         isListAll:false
     })    
+    const [value,setValue] = useState({
+        title:"",
+        description:"",
+        image:""        
+    });
+
+
+    
 
     useEffect(() => {        
         firebase.auth().onAuthStateChanged(authenticated => {
@@ -21,8 +32,59 @@ const Dashboard = () => {
             }else{
                 setDidRedirect(true)                
             }
-        })
+        })        
     }, [])
+
+    const handleChange = name => event => {        
+        event.preventDefault()
+        setValue({...value,[name]:event.target.value})
+    }
+
+    const chooseFile = event => {        
+        event.preventDefault()
+        setValue({...value, image: event.target.value})
+        console.log(value.image)
+    }
+
+    const onSubmit = async () => {
+        var title = document.getElementById('title').value;
+        var description = document.getElementById('description').value
+        const image = document.getElementById('file').files[0];
+        setValue({
+            ...value,
+            image,
+            title,
+            description
+        })        
+
+        const dbReference = firebase.database().ref()
+        const storageRef = firebase.storage().ref()
+        var url = "empty"
+        if(image !== ""){
+            const downloadUrl = await uploadImageAsync(image, storageRef)                        
+            url = downloadUrl
+        }
+
+        var contact = {
+            title:title,
+            description: description,
+            imageDownloadUrl:url
+          }
+          await dbReference.push(contact, err => {
+            if(!err){
+              console.log("SUCCESS")              
+            }
+          })
+        
+    }
+
+
+    const uploadImageAsync = async (image, storageRef) => {
+        const ref = storageRef.child('images').child(uuid.v4())
+        const snapshot = await ref.put(image)
+        return await snapshot.ref.getDownloadURL()
+
+      };
 
     const PerformRedirect = () => {
         return( <Redirect to="/admin/signin" /> )
@@ -37,11 +99,20 @@ const Dashboard = () => {
     const AddNews = () => {
         return(
             <div class="jumbotron">
-            <h1 class="display-4">Add News</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4" />
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <form>
+                    <div class="row">
+                        <div class="col">
+                        <input type="text" className="form-control"  placeholder="Title" id="title" />
+                        </div>
+                        <div class="col">
+                        <textarea type="text" className="form-control" placeholder="Description" id="description" />
+                        </div>                        
+                    </div>
+                    <div className="col-12">                    
+                    <input type="file" class="form-control" id="file" />
+                    </div>
+                    <button class="btn btn-lg btn-primary btn-block text-uppercase" onClick={onSubmit}>Sign in</button>
+                    </form>
             </div>
         )
     }
--- src/ReusableComponents/Main.js
+++ src/ReusableComponents/Main.js
@@ -41,7 +41,7 @@ const Main = ({
   }
   return (
     <div className="backcolor"> 
-      <Slideshow/>  
+      {Slideshow()}
       <div className="">
         <Menu />
       </div>     
