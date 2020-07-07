a71ba3fccecb2494114fe2248e57198394883823
unknown <yashkalavadiya1010@gmail.com>
2020-07-07 13:02:23 +0530

    insta story feature added

--- src/ReusableComponents/MoreUpdates.js
+++ src/ReusableComponents/MoreUpdates.js
@@ -10,9 +10,14 @@ import './CSS/test.css'
 export default function MoreUpdates(props) {
     const [data,setData] = useState([])
     const [isLoading,setIsLoading] = useState(false)
+    const [isFull,setIsFull] = useState(false)
     const stories = [{ url: 'https://picsum.photos/1080/1920',seeMore: ({ close }) => (
         <div onClick={close}>Hello</div>
-    ), header: { heading: 'Mohit Karekar', subheading: 'Posted 5h ago', profileImage: 'https://picsum.photos/1000/1000' } }, { url: 'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN', header: { heading: 'Mohit Karekar', subheading: 'Posted 32m ago', profileImage: 'https://picsum.photos/1080/1920' } }, { url: 'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg', header: { heading: 'mohitk05/react-insta-stories', subheading: 'Posted 32m ago', profileImage: 'https://avatars0.githubusercontent.com/u/24852829?s=400&v=4' } }, { url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4', type: 'video', duration: 1000 }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4', type: 'video' }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', type: 'video' }, 'https://images.unsplash.com/photo-1534856966153-c86d43d53fe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80']
+    ), header: { 
+    heading: 'Mohit Karekar', 
+    subheading: 'Posted 5h ago', 
+    profileImage: 'https://picsum.photos/1000/1000' } }, 
+    { url: 'https://fsa.zobj.net/crop.php?r=dyJ08vhfPsUL3UkJ2aFaLo1LK5lhjA_5o6qEmWe7CW6P4bdk5Se2tYqxc8M3tcgYCwKp0IAyf0cmw9yCmOviFYb5JteeZgYClrug_bvSGgQxKGEUjH9H3s7PS9fQa3rpK3DN3nx-qA-mf6XN', header: { heading: 'Mohit Karekar', subheading: 'Posted 32m ago', profileImage: 'https://picsum.photos/1080/1920' } }, { url: 'https://media.idownloadblog.com/wp-content/uploads/2016/04/iPhone-wallpaper-abstract-portrait-stars-macinmac.jpg', header: { heading: 'mohitk05/react-insta-stories', subheading: 'Posted 32m ago', profileImage: 'https://avatars0.githubusercontent.com/u/24852829?s=400&v=4' } }, { url: 'https://storage.googleapis.com/coverr-main/mp4/Footboys.mp4', type: 'video', duration: 1000 }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4', type: 'video' }, { url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', type: 'video' }, 'https://images.unsplash.com/photo-1534856966153-c86d43d53fe0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80']
 
     const getNews = async () => {
         setIsLoading(true)
@@ -71,13 +76,23 @@ export default function MoreUpdates(props) {
             ))}
             </div>
             {console.log(data)}
-            <Fullscreen enabled={false}>
+            <button onClick={e => setIsFull(true)}>Full</button>
+            <Fullscreen enabled={isFull}>
+                <div className="row">
+                    <div className="col-6">
             <Stories 
         stories={stories}
         defaultInterval={1500}
         width={432}
         height={768}
-      />
+      />      
+      </div>
+      <div className="col-6">
+      <button onClick={e => setIsFull(false)}>Exit</button>
+      </div>
+      </div>
+      </Fullscreen>
+
       <div class="scrolling-wrapper">          
     <img src="https://picsum.photos/1000/1000" className="rounded-circle" style={{width:50, height:50, margin:2}} />
     <img src="https://picsum.photos/1000/1000" className="rounded-circle" style={{width:50, height:50, margin:2}} />
@@ -90,7 +105,6 @@ export default function MoreUpdates(props) {
     <img src="https://picsum.photos/1000/1000" className="rounded-circle" style={{width:50, height:50, margin:2}} />
     <img src="https://picsum.photos/1000/1000" className="rounded-circle" style={{width:50, height:50, margin:2}} />    
     </div>
-      </Fullscreen>
         </Main>
     )
 }
--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -3,12 +3,57 @@ import Main from '../../ReusableComponents/Main';
 import NewsCarousel from '../../ReusableComponents/Carousel';
 import ScrollAnimation from 'react-animate-on-scroll';
 import './Home.css'
+import { useState } from 'react';
+import './Story.css'
+import Stories from 'react-insta-stories';
+import Fullscreen from "react-full-screen";
+import { useEffect } from 'react';
 
 
 
 function Home() {
+  const [currentStory,setCurrentStory] = useState([])
+  const [isStoryVisible,setIsStoryVisible] = useState(false)
+  var touches = 0
+  const storyData = [
+    {url:'https://picsum.photos/1000/1000', header:{
+      heading:"Test",
+      subheading:'Posted 2h ago',
+      profileImage:'https://picsum.photos/1000/1000'
+    }}
+  ]
+
+  useEffect(() => {
+    
+  }, [])
   return (
-      <Main isSlideShow={true}>
+      <Main isSlideShow={true}>        
+          <Fullscreen enabled={isStoryVisible} onChange={f => setIsStoryVisible(f)}>
+            {isStoryVisible && (
+              <div onTouchMove={e => {
+                e.preventDefault()
+                  touches += 16;
+                  console.log(touches)
+                  if(touches > 500){
+                    setIsStoryVisible(false)                
+                  }
+                }}                
+                >
+              <Stories
+              stories={currentStory}
+              defaultInterval={2000}
+              width={window.outerWidth}
+              height={window.outerHeight}              
+            />
+            </div>
+            )}
+        </Fullscreen>        
+        <div class="scrolling-wrapper">          
+          <img src={storyData[0].url} className="rounded-circle" style={{width:50, height:50, margin:2}} onClick={() => {
+            currentStory.push(storyData[0])
+            setIsStoryVisible(true)
+            }} />
+        </div>
 
     <div>            
       <div >
