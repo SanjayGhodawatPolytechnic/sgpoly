853ef674ff57fda51734ff06a8c595a4ed6d529a
unknown <yashkalavadiya1010@gmail.com>
2020-07-10 22:20:28 +0530

    story form completed

--- src/Admin/screens/AddStory.js
+++ src/Admin/screens/AddStory.js
@@ -2,63 +2,72 @@ import React, { useState, useEffect } from 'react';
 import Main from '../../ReusableComponents/Main';
 import { Link } from 'react-router-dom';
 import './CSS/AddStaffMember.css'
+import * as firebase from 'firebase'
+import uuid from 'react-native-uuid';
+
 var _ = require('lodash');
 
 const AddStory = () => {
 
-    const [inputCount,setInputCount] = useState(1)    
+    const [inputCount,setInputCount] = useState({
+        count:1,
+        currentOpen: 1
+    })    
 
-    const InputStory = () => {
-         return _.times(inputCount, (i) => {
-            
-            return(
-            <div class="form-group">
-            <div class="row">            
-                <div className="col-lg-12 m-2">
-                    <button 
-                    class="btn btn-primary w-100" 
-                    type="button" 
-                    data-toggle="collapse" 
-                    data-target={`#multiCollapseExample${i}`} 
-                    aria-expanded="false" 
-                    aria-controls={`multiCollapseExample${i}`}                    
-                    >Story Item {i + 1}</button>
-                </div>
-            <div class="col-lg-12 collapse multi-collapse m-2" id={`multiCollapseExample${i}`}>                                                        
-                    <div class="col-lg-12"><input type="text" class="form-control" name="full_name" placeholder="Enter Heading" required="required" /></div>                                                
-                    <div class="col-lg-12"><input type="text" class="form-control" name="full_name" placeholder="Enter Sub Heading" required="required" /></div>                                                
-                    <div class="col-lg-12"><input type="text" class="form-control" name="full_name" placeholder="Type EG: video or image" required="required" /></div>                                                
-                    <div className="col-lg-12">
-                    <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <label class="input-group-text" for="inputGroupSelect01">Profile Image</label>
-                                </div>
-                                <input 
-                                className="form-control" 
-                                type="file" 
-                                id="profileResume" 
-                                
-                                />
-                        </div>
-                    </div>
-                    <div className="col-lg-12">
-                    <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <label class="input-group-text" for="inputGroupSelect01">Select Media</label>
-                                </div>
-                                <input 
-                                className="form-control" 
-                                type="file" 
-                                id="profileResume" 
-                                
-                                />
-                        </div>
-                    </div>
-            </div>                            
-            </div>
-            </div>            
-        )})                                        
+    const [storyData,setStoryData] = useState({
+        title:'',
+        heading: [],
+        subHeading: [],
+        type: [],
+        profile: [],
+        media: [],
+        snippet: []
+    })
+
+    const submit = async () => {
+        const storyID = uuid.v4()
+        const dbRef = firebase.database().ref('story')
+        const storageRef = firebase.storage().ref()
+        
+
+        const s = {
+            heading:storyData.title
+        }
+
+        await dbRef.child(storyID).push(s,err => {
+            console.error('ERROR OCCURED: ',err.message)
+        }).then(async () => {
+            for(let index = 0; index < storyData.heading.length; index++){
+                let profileURL = 'empty'
+                let mediaURL = 'empty'
+    
+                if(storyData.profile[index] !== 'empty'){
+                    profileURL = await uploadProfileAsync(storyData.profile[index],storageRef)
+                }
+                if(storyData.profile[index] !== 'empty'){
+                    mediaURL = await uploadMediaAsync(storyData.media[index],storageRef)
+                }
+            }
+
+            const data = {
+
+            }
+
+        })    
     }
+
+    const uploadProfileAsync = async (profile, storageRef) => {
+        const ref = storageRef.child('story').child('profile').child(uuid.v4())
+        const snapshot = await ref.put(profile)
+        return await snapshot.ref.getDownloadURL()
+      };
+
+    const uploadMediaAsync = async (media, storageRef) => {
+        const ref = storageRef.child('story').child('media').child(uuid.v4())
+        const snapshot = await ref.put(media)
+        return await snapshot.ref.getDownloadURL()
+      };
+    
 
     return (
         <Main className="container-lg bg-dark roundedd border border-light"> 
@@ -81,15 +90,175 @@ const AddStory = () => {
                     </div>
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-lg-12"><input type="text" class="form-control" name="full_name" placeholder="Enter Main title" required="required" /></div>                            
+                            <div class="col-lg-12"><input 
+                            type="text" 
+                            class="form-control"                             
+                            placeholder="Enter Main title" 
+                            required="required" 
+                            value={storyData.title}
+                            onChange={e => {
+                                setStoryData({
+                                    ...storyData,
+                                    title: e.target.value
+                                })
+                                //console.log(storyData.title)
+                            }}
+                            /></div>                            
                         </div>        	
                     </div>                    
                     
-                            {InputStory()}                    
+
+
+                    {/* ------------------------------------Main Form----------------------------------- */}
+
+                    {_.times(inputCount.count,(i) => (
+
+                            <div class="form-group" key={i}>
+                            <div class="row">            
+                                <div className="col-lg-12 m-2">
+                                    <button 
+                                    class="btn btn-primary w-100" 
+                                    type="button" 
+                                    // data-toggle="collapse" 
+                                    // data-target={`#multiCollapseExample${i}`} 
+                                    // aria-expanded="false" 
+                                    // aria-controls={`multiCollapseExample${i}`}                                        
+                                    onClick={e => {
+                                        e.preventDefault()
+                                        if(inputCount.currentOpen !== i){
+                                        setInputCount({...inputCount, currentOpen: i})
+                                        }else{
+                                            setInputCount({...inputCount, currentOpen: -1})
+                                        }
+                                    }}
+                                    >Story Item {i + 1}</button>
+                                </div>
+                            <div class="col-lg-12 multi-collapse m-2" class={`col-lg-12 multi-collapse m-2 ${inputCount.currentOpen === i ? '' : 'collapse'}`} id={`multiCollapseExample${i}`}>                                                        
+                                    <div class="col-lg-12"><input 
+                                    type="text" 
+                                    class="form-control"                     
+                                    placeholder="Enter Heading"                     
+                                    value={storyData.heading[i] || ''}
+                                    onChange={e => {
+                                        e.preventDefault()
+                                        let heading = storyData.heading
+                                        heading[i] = e.target.value
+                                        setStoryData({...storyData,heading})                        
+                                        //console.log(storyData.heading)
+                                    }}
+                                    />
+                                    </div>                                                
+                                    <div class="col-lg-12"><input 
+                                    type="text" 
+                                    class="form-control"                     
+                                    placeholder="Enter Sub Heading"                     
+                                    value={storyData.subHeading[i] || ''}
+                                    onChange={e => {
+                                        e.preventDefault()
+                                        let subHeading = storyData.subHeading
+                                        subHeading[i] = e.target.value
+                                        setStoryData({
+                                            ...storyData,
+                                            subHeading
+                                        })
+                                        //console.log(storyData.subHeading)                                        
+                                    }}
+                                    /></div>                                                
+                                    <div class="col-lg-12"><input 
+                                    type="text" 
+                                    class="form-control"                     
+                                    placeholder="Type EG: video or image"                     
+                                    value = {storyData.type[i] || ''}
+                                    onChange = {e => {                        
+                                        let type = storyData.type
+                                        type[i] = e.target.value
+                                        setStoryData({
+                                            ...storyData,
+                                            type
+                                        })
+                                        //console.log(storyData.type)
+                                    }}                    
+                                    /></div>
+                                    <div className="col-lg-12">
+                                    <div class="input-group mb-3 m-2">
+                                                <div class="input-group-prepend">
+                                                    <label class="input-group-text" for="inputGroupSelect01">Profile Image</label>
+                                                </div>
+                                                <input 
+                                                className="form-control" 
+                                                type="file" 
+                                                id="profileResume" 
+                                                
+                                                onChange={e => {
+                                                    let profile = storyData.profile
+                                                    profile[i] = e.target.files[0] ? e.target.files[0] : 'empty'
+                                                    setStoryData({
+                                                        ...storyData,
+                                                        profile
+                                                    })
+                                                    //console.log(storyData.profile)
+                                                }}
+                                                />
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-12">
+                                    <div class="input-group mb-3 m-2">
+                                                <div class="input-group-prepend">
+                                                    <label class="input-group-text" for="inputGroupSelect01">Select Media</label>
+                                                </div>
+                                                <input 
+                                                className="form-control" 
+                                                type="file" 
+                                                id="profileResume" 
+                                                onChange={e => {
+                                                    let media = storyData.media
+                                                    media[i] = e.target.files[0] ? e.target.files[0] : 'empty'
+                                                    setStoryData({
+                                                        ...storyData,
+                                                        media
+                                                    })
+                                                    //console.log(storyData.media)
+                                                }}
+                                                />
+                                        </div>
+                                        <div class="col-lg-12"><textarea 
+                                        rows="4" 
+                                        type="text" 
+                                        class="form-control"                         
+                                        placeholder="Enter JSX snippet with bootstrap classes only. *note: this snippet will be displayed with story" 
+                                        value={storyData.snippet[i] || ''}
+                                        onChange={e => {
+                                            let snippet = storyData.snippet
+                                            snippet[i] = e.target.value
+                                            setStoryData({
+                                                ...storyData,
+                                                snippet
+                                            })
+                                            console.log(storyData.snippet)
+                                        }}
+                                        /></div>
+                                        <div className="row">
+                                            <div className="col-lg-12">
+                                                <button className="btn btn-outline-dark">Upload</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>                            
+                            </div>
+                            </div>
+
+                    ))}
+
+
                     <div className="form-group">
                         <div className="row">
                             <div className="col-lg-12">
-                                <button className="btn btn-outline-danger w-100" onClick={() => setInputCount(inputCount+1)}>
+                                <button className="btn btn-outline-danger w-100" onClick={(e) => {
+                                    e.preventDefault()
+                                    
+                                    setInputCount({...inputCount, count: inputCount.count + 1})}
+                                }
+                                    >
                                     <i class="fas fa-plus"></i>
                                 </button>
                             </div>
@@ -97,8 +266,16 @@ const AddStory = () => {
                     </div>                                                                                                
                     <div class="form-group">
                         <div className="row">
-                            <div className="col-lg-12"><button type="submit" class="btn btn-success btn-lg btn-block"                                                        
-                            >Add Story</button></div>
+                            <div className="col-lg-12">
+                                <button 
+                                type="submit" 
+                                class="btn btn-success btn-lg btn-block"                                                        
+                                onClick={e => {
+                                    e.preventDefault()
+                                    submit()
+                                }}
+                                >Add Story</button>
+                            </div>
                         </div>
                     </div>
                 </form>                
