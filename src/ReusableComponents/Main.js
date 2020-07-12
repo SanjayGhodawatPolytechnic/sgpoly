cf1debbc645bc820cf056ea46e5db297d53dd8dd
chintan0539 <chintanpatel0539@gmail.com>
2020-07-12 19:41:19 +0530

    bugggggg

--- src/ReusableComponents/Main.js
+++ src/ReusableComponents/Main.js
@@ -1,45 +1,44 @@
-import React from 'react';
+import React from "react";
 
-
-import Footer from './Footer';
-import MenuBar from './Menu';
-
+import Footer from "./Footer";
+import MenuBar from "./Menu";
 
 const Main = ({
-    title="My Title",
-    description="My description",
-    className="container-lg",    
-    children,
-    isSlideShow=false,
+  title = "My Title",
+  description = "My description",
+  className = "container-lg",
+  children,
+  isSlideShow = false,
 }) => {
-
-
-  const Slideshow=()=>{
-    if(isSlideShow){
-      return(
+  const Slideshow = () => {
+    if (isSlideShow) {
+      return (
         <div className="container">
-          <img src="../img/banner.png" alt=""/>
+          <img src="../img/banner.png" alt="" />
         </div>
-      )
+      );
     }
-  }
-  return (          
-    <div className="backcolor">           
-     
-     <br/>     
-    <div><img src={require("../img/banner.png")} alt="" className="img-fluid"/></div>
-        <br/><br/>
-      <div className="">              
-        <MenuBar />     
-      </div>   
-      <br/><br/>  
-      
-     <div className={className} id="page-wrap" >     
+  };
+  return (
+    <div className="backcolor">
+      <br />
+      <div>
+        <img src={require("../img/banner.png")} alt="" className="img-fluid" />
+      </div>
+      <br />
+      <br />
+      <div className="">
+        <MenuBar />
+      </div>
+      <br />
+      <br />
+
+      <div className={className} id="page-wrap">
         {children}
       </div>
-      <Footer/>
-    </div>    
+      <Footer />
+    </div>
   );
-}
+};
 
 export default Main;
