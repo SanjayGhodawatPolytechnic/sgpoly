731cf4c7c2b6b26c4ca693daf797ca79a338b724
unknown <yashkalavadiya1010@gmail.com>
2020-08-01 17:21:08 +0530

    achivements aved in db

--- src/Admin/screens/ManageAchivements.js
+++ src/Admin/screens/ManageAchivements.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Main from '../../ReusableComponents/Main';
+import { useState } from 'react';
+import * as firebase from 'firebase';
+import uuid from "react-native-uuid";
+import ReactLoading from "react-loading";
 
 const ManageAchivements = () => {
+
+    const [data, setData] = useState([])
+    const [newData, setNewData] = useState({
+        title: '',
+        description: '',
+        image: File,
+        file: File,
+        category: '',
+        dept: '',
+        dateAchived: ''
+    })
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        const dbRef = firebase.database().ref('achivements')
+        const storageref = firebase.storage().ref('achivements')
+        var imageURL = 'empty'
+        var fileURL = 'empty'
+
+        if(newData.image !== '') {
+            imageURL = await UploadImage(storageref, newData.image);
+        }
+        if(newData.file !== "") {
+            fileURL = await UploadFile(storageref, newData.file);
+        }
+
+        const data = {
+            title: newData.title,
+            description: newData.description,
+            category: newData.category,
+            dept: newData.dept,
+            dateAchived: newData.dateAchived,
+            imageURL: imageURL,
+            fileURL: fileURL
+        }
+
+        await dbRef.push(data, (err) => {
+            if(!err){
+                console.log('ADDED');
+                setNewData({
+                    title: '',
+                    description: '',
+                    category: '',
+                    dateAchived: '',
+                    image: null,
+                    file: null,
+                    dept: ''
+                })
+                setIsLoading(false)
+            }
+        })
+    }
+
+    const UploadImage = async (storageref, image) => {
+        const ref = storageref.child('images').child(uuid.v4())
+        const snapshot = await ref.put(image);
+        return snapshot.ref.getDownloadURL();
+    }
+    const UploadFile = async (storageref, file) => {
+        const ref = storageref.child('files').child(uuid.v4())
+        const snapshot = await ref.put(file);
+        return snapshot.ref.getDownloadURL();
+    }
 
     const LeftPanel = () => (
         <div className="container">
@@ -32,16 +101,36 @@ const ManageAchivements = () => {
     const RightPanel = () => (
         <div className="container">
             <form className="md-form">
+                {isLoading && (
+                    <div className="md-form">
+                        <ReactLoading type="spinningBubbles" color="#FFF" />
+                    </div>
+                )}
                 <div class="md-form">
-                    <input type="text" id="form1" class="form-control" />
+                    <input 
+                    type="text" 
+                    id="form1" 
+                    className="form-control"
+                    value={newData.title}
+                    onChange={(e) => {
+                        
+                    setNewData({...newData, title: e.target.value})}} />
                     <label for="form1">Title</label>
                 </div>
                 <div class="md-form">
-                    <input type="text" id="form2" class="form-control" />
+                    <input 
+                    type="text" 
+                    id="form2" 
+                    className="form-control" 
+                    value={newData.description}
+                    onChange={(e) => setNewData({...newData, description: e.target.value})} />
                     <label for="form2">Description</label>
                 </div>
                 <div class="md-form">
-                <select class="browser-default custom-select bg-transparent">
+                <select 
+                className="browser-default custom-select bg-transparent" 
+                value={newData.category}                
+                onChange={(e) => setNewData({...newData, category:e.target.options[e.target.selectedIndex].value})}>
                     <option value="" disabled selected>Achived By</option>
                     <option value="college">College</option>
                     <option value="student">Student</option>
@@ -49,7 +138,7 @@ const ManageAchivements = () => {
                 </div>
                 
                 <div className="md-form">
-                    <select class="browser-default custom-select bg-transparent">
+                    <select class="browser-default custom-select bg-transparent" onChange={(e) => setNewData({...newData, dept:e.target.options[e.target.selectedIndex].value})}>
                         <option value="" disabled selected>Department</option>
                         <option value="CSE">Computer Science</option>
                         <option value="MECH">Mechanical</option>
@@ -59,28 +148,29 @@ const ManageAchivements = () => {
                     </select>
                 </div>
                 <div id="date-picker-example" class="md-form md-outline input-with-post-icon datepicker">
-                    <input placeholder="Select date" type="date" id="example" class="form-control" />
+                    <input placeholder="Select date" type="date" id="example" class="form-control" onChange={(e) => setNewData({...newData, dateAchived: e.target.value})} />
                     <label for="example">Achived on...</label>
                 </div>
                 
+                
                 <div class="md-form">
                 <div class="file-field">
-                    <div class="btn btn-outline-white waves-effect btn-sm float-left">
-                        <span>Image<i class="fas fa-cloud-upload-alt ml-3" aria-hidden="true"></i></span>
-                        <input type="file" className="purple-gradient" />
+                    <div className="row m-2">
+                        <span className="text-muted m-2">Input Image:</span>
+                        <input type="file" id="imagepick" className="purple-gradient" onChange={(e) => setNewData({...newData, image: e.target.files[0]})} />
                     </div>
                 </div>
                 </div>
                 <div class="md-form">
                 <div class="file-field">
-                    <div class="btn btn-outline-white waves-effect btn-sm float-left">
-                        <span>File<i class="fas fa-cloud-upload-alt ml-3" aria-hidden="true"></i></span>
-                        <input type="file" className="purple-gradient" />
+                    <div className="row m-2">
+                        <span className="text-muted m-2">Input File:</span>
+                        <input type="file" id="filepick" className="purple-gradient" onChange={(e) => setNewData({...newData, file: e.target.files[0]})} />
                     </div>
                 </div>
                 </div>
                 <div class="md-form">
-                <button class="btn purple-gradient w-100">Upload Activity</button>
+                <button class="btn purple-gradient w-100" onClick={e => onSubmit(e)}>Upload Activity</button>
                 </div>
             </form>
         </div>
@@ -89,7 +179,7 @@ const ManageAchivements = () => {
         <Main className="bg-dark text-light rounded">
             <div className="row">
                 <div className="col-lg-6"><LeftPanel /></div>
-                <div className="col-lg-6"><RightPanel /></div>
+                <div className="col-lg-6">{RightPanel()}</div>
             </div>
         </Main>
     );
