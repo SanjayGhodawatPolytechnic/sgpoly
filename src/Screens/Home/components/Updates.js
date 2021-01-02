7222d8fa8410a976b4b2da432300bc75e84e22a2
chintan0539 <chintanpatel0539@gmail.com>
2021-01-02 20:45:13 +0530

    aaa

--- src/Screens/Home/components/Updates.js
+++ src/Screens/Home/components/Updates.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Updates.css"
 const Updates = () => {
     return (
-        <div>
+        <div >
             {/* <div id="Artboard__1">
                 <svg class="Rectangle_12">
                     <rect id="Rectangle_12" rx="45" ry="45" x="0" y="0" width="847" height="451">
@@ -26,6 +26,27 @@ const Updates = () => {
                     <span>KNOW MORE</span>
                 </div>
             </div> */}
+            <div className="">
+                <div className="update-card ">
+                <div className="img-cont">
+                    <img className="update-img" src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/images%2F32085ed0-fe21-4303-b02f-645d32a9e30c?alt=media&token=185cdd54-293f-44fc-8c53-2ca7a3a32a6d" />
+                </div>
+                <div className="card-content">
+                    <div className="card-content-title">
+                        <span>Lorem Ipsum Title</span>
+                    </div>
+                    <div className="card-content-desc">
+                        <span>College is arranging a trip to Goa. So, those students who are interested can register their name to their respective departments.</span>
+                    </div>
+                    <div className="POSTED_ON">
+                        <span>POSTED ON: </span><span>1-1-2021</span>
+                    </div>
+                    <div className="KNOW_MORE">
+                        <span>KNOW MORE</span>
+                    </div>
+                </div>
+
+            </div>
             <div className="update-card">
                 <div className="img-cont">
                     <img className="update-img" src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/images%2F32085ed0-fe21-4303-b02f-645d32a9e30c?alt=media&token=185cdd54-293f-44fc-8c53-2ca7a3a32a6d" />
@@ -46,6 +67,9 @@ const Updates = () => {
                 </div>
 
             </div>
+
+            </div>
+            
         </div>
     );
 }
