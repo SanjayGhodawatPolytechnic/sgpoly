e3508852ce37f6a004755a649a51c213696ddba8
yash10102002 <yashkalavadiya1010@gmail.com>
2021-01-06 22:37:41 +0530

    updates card updated :)

--- src/Screens/Home/components/Updates.js
+++ src/Screens/Home/components/Updates.js
@@ -2,53 +2,175 @@ import React from 'react';
 import "./Updates.css"
 const Updates = () => {
     return (
-        <div >
-            <div className="updates">
-                <div className="update-card ">
-                <div className="img-cont">
-                    <img className="update-img" alt="update1" src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/images%2F32085ed0-fe21-4303-b02f-645d32a9e30c?alt=media&token=185cdd54-293f-44fc-8c53-2ca7a3a32a6d" />
-                </div>
-                <div className="card-content">
-                    <div className="card-content-title">
-                        <span>Lorem Ipsum Title</span>
-                    </div>
-                    <div className="card-content-desc">
-                        <span>College is arranging a trip to Goa. So, those students who are interested can register their name to their respective departments.</span>
-                    </div>
-                    <div className="POSTED_ON">
-                        <span>POSTED ON: </span><span>1-1-2021</span>
-                    </div>
-                    <div className="KNOW_MORE">
-                        <span>KNOW MORE</span>
-                    </div>
-                </div>
+        <div class="updates">
+            <div class="site__wrapper">
+      <div class="grid">
+          <div class="card">
+            <div class="card__image">
+              <img src="https://unsplash.it/400/608?image=123" alt="" />
 
-            </div>
-            <div className="update-card">
-                <div className="img-cont">
-                    <img className="update-img" alt="update2" src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/images%2F32085ed0-fe21-4303-b02f-645d32a9e30c?alt=media&token=185cdd54-293f-44fc-8c53-2ca7a3a32a6d" />
-                </div>
-                <div className="card-content">
-                    <div className="card-content-title">
-                        <span>Lorem Ipsum Title</span>
-                    </div>
-                    <div className="card-content-desc">
-                        <span>College is arranging a trip to Goa. So, those students who are interested can register their name to their respective departments.</span>
-                    </div>
-                    <div className="POSTED_ON">
-                        <span>POSTED ON: </span><span>1-1-2021</span>
-                    </div>
-                    <div className="KNOW_MORE">
-                        <span>KNOW MORE</span>
-                    </div>
-                </div>
+              <div class="card__overlay card__overlay--indigo">
+                <div class="card__overlay-content">
+                  <ul class="card__meta">
+                    <li><a href="#0"><i class="fa fa-tag"></i> Html5/Css3</a></li>
+                    <li><a href="#0"><i class="fa fa-clock-o"></i> 2 min ago</a></li>
+                  </ul>
 
-            </div>
+                  <a href="#0" class="card__title">How to create a card based article with HTML5 &amp; CSS3</a>
 
+                  <ul class="card__meta card__meta--last">
+                    <li><a href="#0"><i class="fa fa-user"></i> Mithicher</a></li>
+                    <li><a href="#0"><i class="fa fa-facebook-square"></i> Share</a></li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            
+          </div>
+      </div>
+
+      <div class="grid">
+          <div class="card">
+            <div class="card__image">
+              <img src="https://unsplash.it/400/300?image=100" alt="" />
+
+              <div class="card__overlay card__overlay--blue">
+                <div class="card__overlay-content">
+                  <ul class="card__meta">
+                    <li><a href="#0"><i class="fa fa-tag"></i> UI/UX</a></li>
+                    <li><a href="#0"><i class="fa fa-clock-o"></i> 2 days ago</a></li>
+                  </ul>
+
+                  <a href="#0" class="card__title">How to create a card based article with HTML5 &amp; CSS3</a>
+
+                  <ul class="card__meta card__meta--last">
+                    <li><a href="#0"><i class="fa fa-user"></i> Mithicher</a></li>
+                    <li><a href="#0"><i class="fa fa-facebook-square"></i> Share</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="grid">
+          <div class="card">
+            <div class="card__image">
+              <img src="https://unsplash.it/400/300?image=200" alt="" />
+
+              <div class="card__overlay card__overlay--indigo">
+                <div class="card__overlay-content">
+                <ul class="card__meta">
+                    <li><a href="#0"><i class="fa fa-tag"></i> UI/UX</a></li>
+                    <li><a href="#0"><i class="fa fa-clock-o"></i> 2 days ago</a></li>
+                  </ul>
+
+                  <a href="#0" class="card__title">How to create a card based article with HTML5 &amp; CSS3</a>
+
+                  <ul class="card__meta card__meta--last">
+                    <li><a href="#0"><i class="fa fa-user"></i> Mithicher</a></li>
+                    <li><a href="#0"><i class="fa fa-facebook-square"></i> Share</a></li>
+                  </ul>    
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="grid">
+          <div class="card">
+            <div class="card__image">
+              <img src="https://unsplash.it/400/300?image=300" alt="" />
+
+              <div class="card__overlay card__overlay--indigo">
+                <div class="card__overlay-content">
+                  <ul class="card__meta">
+                    <li><a href="#0"><i class="fa fa-tag"></i> Tutorials</a></li>
+                    <li><a href="#0"><i class="fa fa-clock-o"></i> 2 days ago</a></li>
+                  </ul>
+
+                  <a href="#0" class="card__title">How to create a card based article with HTML5 &amp; CSS3</a>
+
+                  <ul class="card__meta card__meta--last">
+                    <li><a href="#0"><i class="fa fa-user"></i> Mithicher</a></li>
+                    <li><a href="#0"><i class="fa fa-facebook-square"></i> Share</a></li>
+                  </ul>   
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="grid">
+          <div class="card">
+            <div class="card__image">
+              <img src="https://unsplash.it/400/300?image=400" alt="" />
+
+              <div class="card__overlay card__overlay--blue">
+                <div class="card__overlay-content">
+                  <ul class="card__meta">
+                    <li><a href="#0"><i class="fa fa-tag"></i> Tutorials</a></li>
+                    <li><a href="#0"><i class="fa fa-clock-o"></i> 2 days ago</a></li>
+                  </ul>
+
+                  <a href="#0" class="card__title">How to create a card based article with HTML5 &amp; CSS3</a>
+
+                  <ul class="card__meta card__meta--last">
+                    <li><a href="#0"><i class="fa fa-user"></i> Mithicher</a></li>
+                    <li><a href="#0"><i class="fa fa-facebook-square"></i> Share</a></li>
+                  </ul>   
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+</div>
         </div>
     );
 }
 
 export default Updates;
+
+
+// <div className="updates">
+//                 <div className="update-card ">
+//                 <div className="img-cont">
+//                     <img className="update-img" alt="update1" src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/images%2F32085ed0-fe21-4303-b02f-645d32a9e30c?alt=media&token=185cdd54-293f-44fc-8c53-2ca7a3a32a6d" />
+//                 </div>
+//                 <div className="card-content">
+//                     <div className="card-content-title">
+//                         <span>Lorem Ipsum Title</span>
+//                     </div>
+//                     <div className="card-content-desc">
+//                         <span>College is arranging a trip to Goa. So, those students who are interested can register their name to their respective departments.</span>
+//                     </div>
+//                     <div className="POSTED_ON">
+//                         <span>POSTED ON: </span><span>1-1-2021</span>
+//                     </div>
+//                     <div className="KNOW_MORE">
+//                         <span>KNOW MORE</span>
+//                     </div>
+//                 </div>
+
+//             </div>
+//             <div className="update-card">
+//                 <div className="img-cont">
+//                     <img className="update-img" alt="update2" src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/images%2F32085ed0-fe21-4303-b02f-645d32a9e30c?alt=media&token=185cdd54-293f-44fc-8c53-2ca7a3a32a6d" />
+//                 </div>
+//                 <div className="card-content">
+//                     <div className="card-content-title">
+//                         <span>Lorem Ipsum Title</span>
+//                     </div>
+//                     <div className="card-content-desc">
+//                         <span>College is arranging a trip to Goa. So, those students who are interested can register their name to their respective departments.</span>
+//                     </div>
+//                     <div className="POSTED_ON">
+//                         <span>POSTED ON: </span><span>1-1-2021</span>
+//                     </div>
+//                     <div className="KNOW_MORE">
+//                         <span>KNOW MORE</span>
+//                     </div>
+//                 </div>
+
+//             </div>
+
+//             </div>
