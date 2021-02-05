305afd53b1f2f7a8e25888ceec6129c7f4eb4dc5
chintan0539 <chintanpatel0539@gmail.com>
2021-02-05 13:35:28 +0530

    made some amazing

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -9,6 +9,7 @@ import ENTCLogo from "./icons/ENTC";
 import ElectLogo from "./icons/Elect";
 import "./CSS/DeptCard.css"
 import Counter from "./components/Counter";
+import Recents from "./components/Recents"
 
 
 function Home() {
@@ -27,19 +28,19 @@ function Home() {
        
       {/* <Updates /> */}
       <div className="">
-        <br/>
-        <br/>
-        <br />
-        <br/>
+        {/* <br/>
         <br/>
         <br />
         <br/>
         <br/>
         <br />
+        <br/>
+        <br/>
+        <br /> */}
         
        
 
-        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <div id="carouselExampleIndicators" className="carousel slide container margingiv" data-ride="carousel">
         <ol className="carousel-indicators">
           <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active" />
           <li data-target="#carouselExampleIndicators" data-slide-to={1} />
@@ -47,13 +48,13 @@ function Home() {
         </ol>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img  height="600px" className="d-block w-100" src="https://images.unsplash.com/photo-1611338687497-5a9a59910971?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" alt="First slide" />
+            <img  height="600px" className="d-block w-100" src="https://images.unsplash.com/photo-1593642532744-d377ab507dc8?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="First slide" />
           </div>
           <div className="carousel-item">
-            <img  height="600px" className="d-block w-100" src="https://images.unsplash.com/photo-1611332744415-7c20ecb34c11?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" alt="Second slide" />
+            <img  height="600px" className="d-block w-100" src="https://images.unsplash.com/photo-1593642531955-b62e17bdaa9c?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Second slide" />
           </div>
           <div className="carousel-item">
-            <img  height="600px" className="d-block w-100" src="https://images.unsplash.com/photo-1611324679967-a7c2bae60638?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" alt="Third slide" />
+            <img  height="600px" className="d-block w-100" src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80" alt="Third slide" />
           </div>
         </div>
         <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -70,7 +71,7 @@ function Home() {
 
 
 
-
+        <Recents/>
       <Counter />
 {/* 
       <div className="row">
@@ -155,88 +156,10 @@ function Home() {
         </div>
       </div>
 
-      <div className="updates-container">
-        <div className="row update-row">
-          <div className="col-sm-5 update-card shadow-lg">
-            <img className="card-img" src="https://images.unsplash.com/photo-1611324679967-a7c2bae60638?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" alt="Third slide" />
-            HELLO
-          </div>
-          <div className="col-sm-5 update-card shadow-lg">
-            <img className="card-img" src="https://images.unsplash.com/photo-1611324679967-a7c2bae60638?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" alt="Third slide" />
-            HELLO
-          </div>
-        </div>
-      </div>
+      
 
-      <div class="container my-4 updates-container">      
-<div id="multi-item-example" class="carousel slide carousel-multi-item w-100" data-ride="carousel">
-  <div class="controls-top w-100">
-    <a class="btn-floating switch-btn left-btn" href="#multi-item-example" data-slide="prev"><i class="fas fa-chevron-left"></i></a>
-    <a class="btn-floating switch-btn right-btn" href="#multi-item-example" data-slide="next"><i class="fas fa-chevron-right"></i></a>
-  </div>
-  <ol class="carousel-indicators">
-    <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
-    <li data-target="#multi-item-example" data-slide-to="1"></li>
-  </ol>
-  <div class="carousel-inner" role="listbox">
-    <div class="carousel-item active w-100">
-      <div class="col-md-6 w-100" style={{float: "left"}}>
-        <div class="card mb-2 w-100">
-          <img class="card-img-top"
-            src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="slide 1" />
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-              card's content.</p>
-            <a class="btn btn-primary">Button</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 w-100" style={{float: "left"}}>
-        <div class="card mb-2 w-100">
-          <img class="card-img-top"
-            src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="slide 1" />
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-              card's content.</p>
-            <a class="btn btn-primary">Button</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item w-100">
-      <div class="col-md-6 w-100" style={{float: "left"}}>
-        <div class="card mb-2 w-100">
-          <img class="card-img-top"
-            src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg" alt="slide 2" />
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-              card's content.</p>
-            <a class="btn btn-primary">Button</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6 w-100" style={{float: "left"}}>
-        <div class="card mb-2 w-100">
-          <img class="card-img-top"
-            src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg" alt="slide 2" />
-          <div class="card-body">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-              card's content.</p>
-            <a class="btn btn-primary">Button</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div className="w-100 view-more-btn-cont">
-  <button className="more-update-btn">VIEW MORE</button>
-</div>
-</div>
+      
+
     </Main>
   );
 }
