3d6e7315b36b899375a4c413c00e5f8c79c519e9
yash10102002 <yashkalavadiya1010@gmail.com>
2021-02-11 20:11:57 +0530

    fetched pdf through admin api in blob format and rendered in react-pdf, but need to add some UI elements to navigate pdf

--- src/Screens/Departments/Reusables/PDFModal.js
+++ src/Screens/Departments/Reusables/PDFModal.js
@@ -8,37 +8,62 @@ const PDFModal = ({url = "", closePDF}) => {
     
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
+    const [file, setFile] = useState(null)
   
     function onDocumentLoadSuccess({ numPages }) {
       setNumPages(numPages);
+    }
+
+    const loadPDF = () => {
+
+        let data = {
+            pdfurl: "https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/files%2F393c1d8c-6dd6-4475-90bd-ef8e3384b9f8?alt=media&token=27da61ac-2325-404d-98a1-df4168366590"
+        }
+        
+        fetch("http://localhost:3002/api/getPDF", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(async (res) => {
+            let blb = await res.blob()
+            let pdffile = new File([blb], "testfile.pdf");
+            console.log(pdffile);
+            setFile(pdffile)
+        })
+        .catch(err => {
+            console.log(err);
+        })
     }
 
     useEffect(() => {
         console.log(numPages)
     }, [numPages])
     
+    useEffect(() => {
+        loadPDF();
+    }, [])
 return (
     <div className="pdf-modal" onClick={closePDF}>
         <div className="close-btn" onClick={closePDF}><i aria-hidden="true" className="fas fa-times-circle fa-2x"></i></div>
-        <div className="container" onClick={e => {
-            e.stopPropagation();
-        }}>
-            <div>
-                
-                <Document
-                    file={{
-                        url: 'https://thingproxy.freeboard.io/fetch/https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/files%2F393c1d8c-6dd6-4475-90bd-ef8e3384b9f8?alt=media&token=27da61ac-2325-404d-98a1-df4168366590'
-                    }}
-                    onLoadSuccess={onDocumentLoadSuccess}
-                    onLoadError={err => {
-                        console.log(err)
-                    }}
-                >
-                    <Page pageNumber={pageNumber} />
-                </Document>
-                <p>Page {pageNumber} of {numPages}</p>
-            </div>            
-            </div>
+        {file && (
+            <div className="container p-0 d-flex justify-content-center align-items-center" onClick={e => {
+                e.stopPropagation();
+            }}>     
+                    <Document
+                        file={file}
+                        onLoadSuccess={onDocumentLoadSuccess}
+                        onLoadError={err => {
+                            console.log(err)
+                        }}
+                    >
+                        <Page pageNumber={pageNumber} />
+                    </Document>
+                    <p>Page {pageNumber} of {numPages}</p>
+                </div>
+        )}
     </div>
 )
         }
