3f78f1b631186a3c5b84bd0d253f99dcbaec0745
yash10102002 <yashkalavadiya1010@gmail.com>
2021-03-19 00:37:07 +0530

    testimonials added

--- src/Screens/Home/components/Imp.js
+++ src/Screens/Home/components/Imp.js
@@ -1,72 +1,68 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Imp.css";
-
+// src="https://images.pexels.com/photos/127513/pexels-photo-127513.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+// src="https://images.pexels.com/photos/631954/pexels-photo-631954.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+// src="https://images.pexels.com/photos/247599/pexels-photo-247599.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
 const Imp = () => {
   return (
     <div>
       <div className="content-wrapper">
         <div className="news-card">
-          <a href="#" className="news-card__card-link" />
           <img
-            src="https://images.pexels.com/photos/127513/pexels-photo-127513.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            src="http://www.sgipolytechnic.in/pics/Chairman2.jpg"
             alt=""
             className="news-card__image"
           />
           <div className="news-card__text-wrapper">
             <h2 className="news-card__title">Mr. Sanjay Ghodawat</h2>
-            <div className="news-card__post-date">Jan 29, 2018</div>
+            <div className="news-card__post-date">Chaiman</div>
             <div className="news-card__details-wrapper">
               <p className="news-card__excerpt">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-                pariatur nemo tempore repellat? Ullam sed officia iure
-                architecto deserunt distinctio, pariatur…
+              Education is making students aware of the infinite potential within and bringing it up so that the result is transformation of personalities.
               </p>
-              <a href="#" className="news-card__read-more">
+              <Link to="/management/chairman" className="news-card__read-more">
                 Read more <i className="fas fa-long-arrow-alt-right" />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
         <div className="news-card">
-          <a href="#" className="news-card__card-link" />
           <img
-            src="https://images.pexels.com/photos/631954/pexels-photo-631954.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+            src="http://www.sgipolytechnic.in/pics/Trustee.jpg"
             alt=""
             className="news-card__image"
           />
           <div className="news-card__text-wrapper">
             <h2 className="news-card__title">Mr. Vinek Bhosale</h2>
-            <div className="news-card__post-date">Jan 29, 2018</div>
+            <div className="news-card__post-date">Trustee</div>
             <div className="news-card__details-wrapper">
               <p className="news-card__excerpt">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam
-                obcaecati ex natus nulla rem sequi laborum quod fugit…
+              If you treat an individual as he is, he will remain how he is. But if you treat him as if he were what he ought to be and could be, he will become what he ought to be and could be.
               </p>
-              <a href="#" className="news-card__read-more">
+              <Link to="/management/trustee" className="news-card__read-more">
                 Read more <i className="fas fa-long-arrow-alt-right" />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
         <div className="news-card">
-          <a href="#" className="news-card__card-link" />
           <img
-            src="https://images.pexels.com/photos/247599/pexels-photo-247599.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            src="http://www.sgipolytechnic.in/pics/Principal.jpg"
             alt=""
             className="news-card__image"
           />
           <div className="news-card__text-wrapper">
             <h2 className="news-card__title">Mr. Virat Giri</h2>
-            <div className="news-card__post-date">Jan 29, 2018</div>
+            <div className="news-card__post-date">Principal</div>
             <div className="news-card__details-wrapper">
               <p className="news-card__excerpt">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Officiis beatae…
+              All the flowers of all the tomorrows are in the in seeds of today.
               </p>
-              <a href="#" className="news-card__read-more">
+              <Link to="/management/principal" className="news-card__read-more">
                 Read more <i className="fas fa-long-arrow-alt-right" />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
