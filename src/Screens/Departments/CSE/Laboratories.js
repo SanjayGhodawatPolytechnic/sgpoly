118b8934c531c748b7d7d2373feb5c6dc668a3b2
Rupam Jadhav <rj1018letsrock@gmail.com>
2021-03-30 22:59:14 +0530

    departments

--- src/Screens/Departments/CSE/Laboratories.js
+++ src/Screens/Departments/CSE/Laboratories.js
@@ -29,10 +29,10 @@ const Laboratories = () => {
               <LabCard
                 labName="Computer Center Lab"
                 labcost="15, 48,233"
-                invoiceurl="http://sgipolytechnic.in/Labs/CSE/cc_lab.pdf"
+                invoiceurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2Fcc_lab.pdf?alt=media&token=cea9f1e1-981a-4a5c-b68d-6ab4faaa7d5c"
                 use="Uses for internet center."
                 description="This lab is equipped with 30 systems of Dell OptiPlex Series With Intel I3 Processor, 3 Printers , 3 scanners and 1 projector. This is central lab of the Polytechnic consisting of DELL SERVER X206 acting as central ERP server."
-                imageurl="http://sgipolytechnic.in/Labs/CSE/CSE1.jpg"
+                imageurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FCSE1.jpg?alt=media&token=9b367970-7f04-4d6a-be75-8733763b3ead"
                 itemno={0}
               />
             </div>
@@ -41,10 +41,10 @@ const Laboratories = () => {
               <LabCard
                 labName="Programming Lab"
                 labcost="9,40,704.00"
-                invoiceurl="http://sgipolytechnic.in/Labs/CSE/PRLABlab.pdf"
+                invoiceurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FPRLABlab.pdf?alt=media&token=53e9aab3-7e47-40e6-aaac-06aa90c23844"
                 use="Uses for programming subjects like C, Java, Databases etc"
                 description="This lab is equipped with 24 systems of Dell OptiPlex Series With Intel I3 Processor with Software's Like C, Java, Oracle, VB 6.0"
-                imageurl="http://sgipolytechnic.in/Labs/CSE/CSE2.jpg"
+                imageurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FCSE2.jpg?alt=media&token=ebf0cb63-0a24-474e-b791-c9cb4bf6d076"
                 itemno={1}
               />
             </div>
@@ -53,10 +53,10 @@ const Laboratories = () => {
               <LabCard
                 labName="Basic Workshop Lab"
                 labcost="6,59,085.00"
-                invoiceurl="http://sgipolytechnic.in/Labs/CSE/basic_lab.pdf"
+                invoiceurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2Fbasic_lab.pdf?alt=media&token=9a98691b-ee91-4442-a523-70ec164740ac"
                 use="Uses for computer hardware demonstration and hardware testing"
                 description="This lab is equipped with 20 systems of Dell OptiPlex Series With Intel I3 Processor , 1 prionter with Hardware Material, Lan Tester, Crimping Tool."
-                imageurl="http://sgipolytechnic.in/Labs/CSE/CSE3.jpg"
+                imageurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FCSE3.jpg?alt=media&token=8f36fe93-6edc-4351-a47a-4d5fbc3a10fd"
                 itemno={3}
               />
             </div>
@@ -65,10 +65,10 @@ const Laboratories = () => {
               <LabCard
                 labName="Network Lab"
                 labcost="9,58,894.00"
-                invoiceurl="http://sgipolytechnic.in/Labs/CSE/NET_lab.pdf"
+                invoiceurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FNET_lab.pdf?alt=media&token=14daba65-576e-4063-a22f-b523c06e5cef"
                 use="Uses for networking related practical's, open source Red Hat Linux, SUSE Server products and database use."
                 description="This lab is equipped with 24 systems of Dell OptiPlex Series with Intel I3 Processor , 24 Port Switch, UTP Cable's & Networking Ports, Sockets."
-                imageurl="http://sgipolytechnic.in/Labs/CSE/CSE4.jpg"
+                imageurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FCSE4.jpg?alt=media&token=fa209875-5153-4343-ab84-c8dde3759f86"
                 itemno={4}
               />
             </div>
@@ -77,10 +77,10 @@ const Laboratories = () => {
               <LabCard
                 labName="Software Testing Lab"
                 labcost="9,95,106.00"
-                invoiceurl="http://sgipolytechnic.in/Labs/CSE/SWT_lab.pdf"
+                invoiceurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FSWT_lab.pdf?alt=media&token=10ff3ff3-b6ed-4158-9726-6050ea444e46"
                 use="Uses for programming subjects software testing and error handlings."
                 description="This lab is equipped with 20 systems of Dell OptiPlex Series with Intel I3 Processor , Software's Like C, Java, Oracle products"
-                imageurl="http://sgipolytechnic.in/Labs/CSE/CSE5.jpg"
+                imageurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FCSE5.jpg?alt=media&token=aedcaf52-4ddd-49d5-820f-db92c10158a6"
                 itemno={5}
               />
             </div>
@@ -89,10 +89,10 @@ const Laboratories = () => {
               <LabCard
                 labName="Linux Lab"
                 labcost="10,39,630.00"
-                invoiceurl="http://sgipolytechnic.in/Labs/CSE/Linux_lab.pdf"
+                invoiceurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FLinux_lab.pdf?alt=media&token=c24e84e0-19fb-4d29-a1de-c418bd533239"
                 use="Uses for Linux server practices and Linux programming subjects"
                 description="This lab is equipped with 20 systems of Dell OptiPlex Series with Intel I3 Processor specially designed For Open Source, Linux server practical's and Linux programming."
-                imageurl="http://sgipolytechnic.in/Labs/CSE/CSE6.jpg"
+                imageurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FCSE6.jpg?alt=media&token=7f259866-97fc-4ce8-8148-2e5581301dfe"
                 itemno={6}
               />
             </div>
@@ -101,10 +101,10 @@ const Laboratories = () => {
               <LabCard
                 labName="Programming Lab 1"
                 labcost="10,76,380.00"
-                invoiceurl="http://sgipolytechnic.in/Labs/CSE/PRLAB_lab1.pdf"
+                invoiceurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FPRLAB_lab1.pdf?alt=media&token=1708eab7-148c-40b8-b1ba-83e8dcd18021"
                 use="Usesd for programming subjects like C, Java, Databases etc."
                 description="This lab is equipped with 20 systems of Dell OptiPlex Series with Intel I3 Processor , software's Like C, Java, Oracle, VB 6.0"
-                imageurl="http://sgipolytechnic.in/Labs/CSE/CSE7.jpg"
+                imageurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FCSE7.jpg?alt=media&token=f739745b-0a0b-4143-8fea-9345bb6a196a"
                 itemno={7}
               />
             </div>
@@ -113,10 +113,10 @@ const Laboratories = () => {
               <LabCard
                 labName="Language Lab"
                 labcost="9,78,006.00 "
-                invoiceurl="http://sgipolytechnic.in/Labs/CSE/language_lab.pdf"
+                invoiceurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2Flanguage_lab.pdf?alt=media&token=c60bf165-b167-4dc7-be23-751cc3a67741"
                 use="Uses for communication skills"
                 description="This lab is equipped with 20 systems of Dell OptiPlex Series with Intel I3 Processor , 1 Printer , 1 Projector , Language Lab Software with headphones"
-                imageurl="http://sgipolytechnic.in/Labs/CSE/CSE8.jpg"
+                imageurl="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FLabs%2FCse%2FCSE8.jpg?alt=media&token=3052090e-72a4-496f-8176-3a7816e822e1"
                 itemno={8}
               />
             </div>
