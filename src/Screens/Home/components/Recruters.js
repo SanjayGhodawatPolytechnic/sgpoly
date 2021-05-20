784c612be807f57336b329dc4218c6b7dee53e68
chintan0539 <chintanpatel0539@gmail.com>
2021-05-20 19:19:29 +0530

    recruit add

--- src/Screens/Home/components/Recruters.js
+++ src/Screens/Home/components/Recruters.js
@@ -1,19 +1,27 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
-import "./ImgCarousel.css";
+import * as firebase from "firebase";
+import { useState } from "react";
+import { useEffect } from "react";
 
 import "./Recruters.css";
 const Recruters = () => {
-  const Image = ({ src, alt }) => (
-    <div className="container p-0 m-0" style={{ width: "auto" }}>
-      <img
-        src={src}
-        alt={alt}
-        style={{ height: "100px", marginRight: "20px" }}
-        className="img img-fluid"
-      />
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  const getRec = async () => {
+    let dataRef = firebase.database().ref("recruiters");
+    dataRef.on("value", (dataSnapshot) => {
+      if (dataSnapshot.val()) {
+        let result = Object.values(dataSnapshot.val());
+        setData(result);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getRec();
+  }, []);
+
   return (
     <div className="img-gallery-container">
       <div className="courses-title">
@@ -22,30 +30,13 @@ const Recruters = () => {
       </div>
       <div class="Marquee">
         <div class="Marquee-content">
-          <div class="Marquee-tag">
-            <img
-              src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
-              alt=""
-            />
-          </div>
-          <div class="Marquee-tag">
-            <img
-              src="https://cdn.vox-cdn.com/thumbor/NeSo4JAqv-fFJCIhb5K5eBqvXG4=/7x0:633x417/1200x800/filters:focal(7x0:633x417)/cdn.vox-cdn.com/assets/1311169/mslogo.jpg"
-              alt=""
-            />
-          </div>
-          <div class="Marquee-tag">
-            <img
-              src="https://1000logos.net/wp-content/uploads/2021/04/Accenture-logo.png"
-              alt=""
-            />
-          </div>
-          <div class="Marquee-tag">
-            <img
-              src="https://cdn.mos.cms.futurecdn.net/5ij5qdSHFzJ2piPRuoTL5F-1200-80.jpg"
-              alt=""
-            />
-          </div>
+          {data.map((d, i) => {
+            return (
+              <div class="Marquee-tag">
+                <img src={d.URL} alt="" />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
