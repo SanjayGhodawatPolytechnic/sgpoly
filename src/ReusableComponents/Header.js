070f78ba00a61d30e98d9d1455cd73456325d403
yash10102002 <yashkalavadiya1010@gmail.com>
2021-05-21 22:55:59 +0530

    few changes

--- src/ReusableComponents/Header.js
+++ src/ReusableComponents/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="banner">
       <img
-        src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/staticFiles%2Fn.jpg?alt=media&token=553d9f12-f90c-4783-82fb-33a8e39b0b7e"
+        src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/srcimg%2FHome%2FBlank-Template-Twitter-Header.jpg?alt=media&token=0aab0ba0-d2d7-450b-9435-63c770b96b0d"
         className="bannner-img"
       />
     </div>
