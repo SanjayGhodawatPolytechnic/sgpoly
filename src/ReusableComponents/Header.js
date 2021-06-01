6a5de7761a46f3f4cfcb02f8f57b38cf9e34ed1e
chintan0539 <chintanpatel0539@gmail.com>
2021-06-01 14:14:36 +0530

    header changed

--- src/ReusableComponents/Header.js
+++ src/ReusableComponents/Header.js
@@ -3,18 +3,8 @@ import "./CSS/header.css";
 const Header = () => {
   return (
     <div className="banner">
-      <div className="contact-info-cont">
-        <div className="phone">
-          <i className="fas fa-phone mr-3 " />
-          0230 - 2463800
-        </div>
-        <div className="email">
-          <i className="fas fa-envelope mr-3" />
-          principal@sgipolytechnic.in
-        </div>
-      </div>
       <img
-        src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/staticFiles%2FUntitled-Project%20(2).jpg?alt=media&token=6dee11cb-3ff9-48df-9f74-47ee66eacbaa"
+        src="https://firebasestorage.googleapis.com/v0/b/sgpoly-86d3b.appspot.com/o/staticFiles%2Ffinal%20banner.jpg?alt=media&token=9399eb99-4e2b-418a-a86f-6301b6e096bc"
         className="bannner-img"
       />
     </div>
